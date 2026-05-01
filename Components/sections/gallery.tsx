5f59f97b2b8ff1d3641698599233aac975d5f0ff
Amin Zare <2005aminzare@gmail.com>
2026-05-01 16:02:33 +0330

    feat: import 3d-card and use it in gallery and work section

--- Components/sections/gallery.tsx
+++ Components/sections/gallery.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import Section from "../section";
 import Image from "next/image";
 import { GALLERY_ITEMS } from "@/constants/items";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 
 function Gallery() {
   return (
     <Section classNameWrapper="columns-1 md:columns-2 gap-2 space-y-2">
       {GALLERY_ITEMS.map((item) => (
-        <div key={item.url} className="break-inside-avoid">
-          <Image
-            draggable={false}
-            src={item.url}
-            alt={item.title}
-            width={300}
-            height={300}
-            loading="lazy"
-            className="w-full saturate-25"
-          />
-        </div>
+        <CardContainer key={item.url} className="break-inside-avoid">
+          <CardBody>
+            <CardItem translateZ={20}>
+              <Image
+                draggable={false}
+                src={item.url}
+                alt={item.title}
+                width={300}
+                height={300}
+                loading="lazy"
+                className="w-full saturate-25"
+              />
+            </CardItem>
+          </CardBody>
+        </CardContainer>
       ))}
     </Section>
   );
--- Components/sections/work.tsx
+++ Components/sections/work.tsx
@@ -4,46 +4,51 @@ import React from "react";
 import Section from "../section";
 import { WORK_ITEMS } from "@/constants/items";
 import { Link2Icon } from "lucide-react";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 
 function Work() {
   return (
     <Section classNameWrapper="flex flex-col gap-4">
       {WORK_ITEMS.map((item, i) => (
-        <div
-          key={i}
-          className="flex flex-col w-full group justify-center items-center gap-2 bg-white p-6 border border-black/10 rounded-xl hover:translate-y-[1px] duration-300"
+        <CardContainer
+          key={item.compony}
+          className="bg-white p-6 border border-black/10 rounded-xl"
         >
-          <div className="flex w-full justify-between">
-            <div className="flex items-center gap-2">
-              <Image
-                src={item.image}
-                alt={item.compony}
-                width={60}
-                height={60}
-                draggable="false"
-                className="size-10 rounded-lg"
-              />
-              <div className="flex flex-col">
-                <b>{item.position}</b>
-                <span className="tracking-wider text-sm font-light opacity-85">
-                  {item.compony}
-                </span>
+          <CardBody className="w-full">
+            <CardItem className="w-full flex flex-col justify-center items-center gap-2">
+              <div className="flex w-full justify-between">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={item.image}
+                    alt={item.compony}
+                    width={60}
+                    height={60}
+                    draggable="false"
+                    className="size-10 rounded-lg"
+                  />
+                  <div className="flex flex-col">
+                    <b>{item.position}</b>
+                    <span className="tracking-wider text-sm font-light opacity-85">
+                      {item.compony}
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="w-full">
-            <p className="opacity-85">{item.description}</p>
-            <Link
-            // replace hidden w flex
-              className="self-start hidden py-1 gap-1 justify-start text-blue-500 items-center text-sm"
-              target="_blank"
-              href={item.link.href}
-            >
-              <Link2Icon size={18} />
-              {item.link.title}
-            </Link>
-          </div>
-        </div>
+              <div className="w-full">
+                <p className="opacity-85">{item.description}</p>
+                <Link
+                  // replace hidden w flex
+                  className="self-start hidden py-1 gap-1 justify-start text-blue-500 items-center text-sm"
+                  target="_blank"
+                  href={item.link.href}
+                >
+                  <Link2Icon size={18} />
+                  {item.link.title}
+                </Link>
+              </div>
+            </CardItem>
+          </CardBody>
+        </CardContainer>
       ))}
     </Section>
   );
